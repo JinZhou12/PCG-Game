ed6917a55beca42b807daf1aab597f1e19233d8a
Jordan Imp <49874843+jsi238@users.noreply.github.com>
2024-10-17 23:29:50 -0400

    Added movement and shooting
    WASD moves player, Arrow Keys shoot bullet. Fires automatically though rapidly smashing the arrow keys will let players fire very fast.

---
+++ PCG Game/Assets/Scripts/Bullet_Controller.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Controller : MonoBehaviour
+{
+    private GameObject player;
+
+    [SerializeField] private float bulletSpeed;
+    [SerializeField] private float lifeTime;
+
+    private float timeAlive = 0;
+
+    private Vector2 direction;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Abraham");
+
+        Player_Controller playerController = player.GetComponent<Player_Controller>();
+
+        if (GetComponent<Rigidbody2D>() != null)
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        direction = playerController.GetDirection();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.gameObject != null)
+        {
+            HandleMovement();
+
+            BulletDeath();
+        }
+    }
+
+    public void BulletDeath()
+    {
+        timeAlive += Time.deltaTime;
+        if (timeAlive > lifeTime)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void SetBulletSpeed(float newBulletSpeed)
+    {
+        bulletSpeed = newBulletSpeed;
+    }
+
+    public void HandleMovement()
+    {
+        Debug.Log(direction);
+        rb.velocity = direction * bulletSpeed;
+    }
+}
--- PCG Game/Assets/Scripts/Player_Controller.cs
+++ PCG Game/Assets/Scripts/Player_Controller.cs
@@ -4,15 +4,148 @@ using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private Animator animator;
+    private Transform pos;
+
+    private Vector2 direction; //direction player is facing
+
+    [SerializeField] private GameObject bulletPrefab;
+    private Bullet_Controller bulletController; 
+
+    [SerializeField] private float moveSpeed; //how fast player moves
+    [SerializeField] private float attackSpeed; //how long before next bullet; shorter is faster
+
+    private bool isMoving = false;
+
+    private float firingTime;
+    private bool isShooting = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        firingTime = attackSpeed + 1; //load one bullet in the chamber
+
+        if (GetComponent<Rigidbody2D>() != null)
+            rb = GetComponent<Rigidbody2D>();
+
+        if (GetComponent<Animator>() != null)
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        if (GetComponent<Transform>() != null)
+        {
+            pos = GetComponent<Transform>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        HandleMovement();
+
+        HandleShoot();
+    }
+
+    public void HandleMovement()
+    {
+        bool moveLeft = Input.GetKey(KeyCode.A);
+        bool moveRight = Input.GetKey(KeyCode.D);
+        bool moveUp = Input.GetKey(KeyCode.W);
+        bool moveDown = Input.GetKey(KeyCode.S);
+
+        if (moveLeft || moveRight || moveUp || moveDown)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+
+        if (isMoving)
+        {
+            if (moveLeft)
+            {
+                pos.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+                direction = Vector2.left;
+            }
+            if (moveRight)
+            {
+                pos.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+                direction = Vector2.right;
+            }
+            if (moveUp)
+            {
+                pos.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+                direction = Vector2.up;
+            }
+            if (moveDown)
+            {
+                pos.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+                direction = Vector2.down;
+            }
+        }
+        else
+        {
+            //rb.velocity = Vector3.zero;
+        }
+    }
+
+    public void HandleShoot()
+    {
+        bool shootLeft = Input.GetKey(KeyCode.LeftArrow);
+        bool shootRight = Input.GetKey(KeyCode.RightArrow);
+        bool shootUp = Input.GetKey(KeyCode.UpArrow);
+        bool shootDown = Input.GetKey(KeyCode.DownArrow);
+
+        if (shootLeft || shootRight || shootUp || shootDown)
+        {
+            isShooting = true;
+        }
+        else
+        {
+            isShooting = false;
+            firingTime = attackSpeed + 1;
+        }
+
+        if (isShooting)
+        {
+            firingTime += Time.deltaTime;
+            if (firingTime >= attackSpeed)
+            {
+                firingTime = 0;
+                if (shootLeft)
+                {
+                    direction = Vector2.left;
+                    Instantiate(bulletPrefab, pos.position, Quaternion.identity);
+                    bulletController = bulletPrefab.GetComponent<Bullet_Controller>();
+                }
+                else if (shootRight)
+                {
+                    direction = Vector2.right;
+                    Instantiate(bulletPrefab, pos.position, Quaternion.identity);
+                    bulletController = bulletPrefab.GetComponent<Bullet_Controller>();
+                }
+                else if (shootUp)
+                {
+                    direction = Vector2.up;
+                    Instantiate(bulletPrefab, pos.position, Quaternion.identity);
+                    bulletController = bulletPrefab.GetComponent<Bullet_Controller>();
+                }
+                else if (shootDown)
+                {
+                    direction = Vector2.down;
+                    Instantiate(bulletPrefab, pos.position, Quaternion.identity);
+                    bulletController = bulletPrefab.GetComponent<Bullet_Controller>();
+                }
+            }
+        }
+    }
+
+    public Vector2 GetDirection()
+    {
+        return direction;
     }
 }
