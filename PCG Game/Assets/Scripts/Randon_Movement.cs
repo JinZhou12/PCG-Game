7373dc93928a1f07f397ee7e0c5bfa5538b5de20
Jordan Imp <49874843+jsi238@users.noreply.github.com>
2024-10-21 00:47:58 -0400

    Enemies that move randomly also take damage

--- PCG Game/Assets/Scripts/Randon_Movement.cs
+++ PCG Game/Assets/Scripts/Randon_Movement.cs
@@ -5,7 +5,9 @@ using UnityEngine.UIElements;
 
 public class Randon_Movement : MonoBehaviour
 {
+    [Header("Enemy Specs")]
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float health;
 
     private Vector2 direction;
     private Vector2[] directions = { Vector2.up, Vector2.down, Vector2.left, Vector2.right };
@@ -16,6 +18,8 @@ public class Randon_Movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        health = Random.Range(1, 6); //enemies with random movement have 1 to 5 health
+
         CreateNewDirection();
     }
 
@@ -32,6 +36,13 @@ public class Randon_Movement : MonoBehaviour
 
         HandleMovement();
     }
+    private void HandleDeath()
+    {
+        if (health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
 
     private void CreateNewDirection()
     {
@@ -45,10 +56,15 @@ public class Randon_Movement : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag != "Player")
+        if (collision.gameObject.tag != "Player" && collision.gameObject.tag != "Bullet")
         {
             Debug.Log("bonk :(");
             direction = -direction;
+        }
+
+        if (collision.gameObject.tag == "Bullet")
+        {
+            health -= collision.gameObject.GetComponent<Bullet_Controller>().GetDamage();
         }
     }
 }
